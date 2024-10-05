1230551a92a411e046ec4ab063d9469774aff8f7
186480 <186480@stud.hvl.no>
2024-10-05 14:44:30 +0200

    op

--- src/GPSprosjekt/Oppg1.java
+++ src/GPSprosjekt/Oppg1.java
@@ -3,8 +3,16 @@ package GPSprosjekt;
 public class Oppg1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String a = "2";
+		int b = 2;
+		int d = 2;
+		String c = a+b;
+		String e = c+b+d;
+		
+		//System.out.println(a+b);
+		System.out.println(e);
 	}
-
 }
+
+	
