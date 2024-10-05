f38ebb87faad045b42de1c768809372955819bcf
186480 <186480@stud.hvl.no>
2024-10-05 16:08:02 +0200

    Create Oppg2.java

---
+++ src/GPSprosjekt/Oppg2.java
@@ -0,0 +1,26 @@
+package GPSprosjekt;
+
+
+
+public class Oppg2 {
+
+	private static int TIME_STARTINDEX = 11; 
+
+	public static int toSeconds(String timestr) {
+		
+		int secs;
+		int hr, min, sec;
+		
+		
+		
+	}
+
+	public static GPSPoint convert(String timeStr, String latitudeStr, String longitudeStr, String elevationStr) {
+
+		GPSPoint gpspoint;
+
+		// TODO 
+		throw new UnsupportedOperationException(TODO.method());
+		
+	}
+}
