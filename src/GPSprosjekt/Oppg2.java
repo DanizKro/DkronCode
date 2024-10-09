64c134acafb487ad947d4d5a150130a5f220d2b2
186480 <186480@stud.hvl.no>
2024-10-09 16:34:52 +0200

    Update Oppg2.java

--- src/GPSprosjekt/Oppg2.java
+++ src/GPSprosjekt/Oppg2.java
@@ -6,21 +6,21 @@ public class Oppg2 {
 
 	private static int TIME_STARTINDEX = 11; 
 
-	public static int toSeconds(String timestr) {
-		
-		int secs;
-		int hr, min, sec;
-		
-		
-		
-	}
-
-	public static GPSPoint convert(String timeStr, String latitudeStr, String longitudeStr, String elevationStr) {
-
-		GPSPoint gpspoint;
-
-		// TODO 
-		throw new UnsupportedOperationException(TODO.method());
-		
-	}
+//	public static int toSeconds(String timestr) {
+//		
+//		int secs;
+//		int hr, min, sec;
+//		
+//		
+//		
+//	}
+//
+//	public static GPSPoint convert(String timeStr, String latitudeStr, String longitudeStr, String elevationStr) {
+//
+//		GPSPoint gpspoint;
+//
+//		// TODO 
+//		throw new UnsupportedOperationException(TODO.method());
+//		
+//	}
 }
