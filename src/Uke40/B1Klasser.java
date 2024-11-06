2122a74240efad0a55017ee3e0ae3babded46539
Dkron <186480@stud.hvl.no>
2024-11-06 13:24:49 +0100

    op

--- src/Uke40/B1Klasser.java
+++ src/Uke40/B1Klasser.java
@@ -3,8 +3,33 @@ package Uke40;
 public class B1Klasser {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		System.out.println();
+		metodeB();
+	}
+		public static void metodeA(int x) {
+			if (x < 0) {
+			throw new ArithmeticException("X er negativ");
+			} else {
+			System.out.print(x + " ");
+			}
+			}
+			public static void metodeB() {
+				
+			int[] tab = { 0, 1, -1, 2, 3 };
+			int x = 0;
+			try {
+			for (int i = 0; i < tab.length; i++) {
+			x = i;
+			metodeA(tab[i]);
+			}
+			} catch (ArithmeticException exception) {
+			System.out.println();
+			System.out.println(tab[x]);
+			} finally {
+			System.out.println("x = " + x);
+			}
 	}
 
 }
